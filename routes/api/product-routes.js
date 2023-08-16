acdefdede696e4b8d39090cc4630cdf219825266
rhonvyramos <rhonvyramos@gmail.com>
2023-08-16 10:57:35 -0400

    delete route for one product implemented

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,8 +114,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product_data = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product_data) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(product_data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
