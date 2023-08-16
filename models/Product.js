36cb7856c446178dfa78254b322326dc2b35c3ae
rhonvyramos <rhonvyramos@gmail.com>
2023-08-16 11:10:47 -0400

    fixed error where category_id model attribute in product model did not have datatype assigned to it; category_id now has the INTEGER datatype

--- models/Product.js
+++ models/Product.js
@@ -38,6 +38,7 @@ Product.init(
       }
     },
     category_id: {
+      type: DataTypes.INTEGER,
       references: {
         model: "category",
         key: "id"
--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-Category.belongsToMany(Product);
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
