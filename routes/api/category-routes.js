d458105f529b8cb95813f3cdb686e407dfaef508
rhonvyramos <rhonvyramos@gmail.com>
2023-08-16 13:43:41 -0400

    delete route added for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -51,8 +51,24 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const category_data = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!category_data) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(category_data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
