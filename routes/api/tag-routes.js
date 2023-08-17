787468edc5c24b481a1c648ced9f09b753c24bdc
rhonvyramos <rhonvyramos@gmail.com>
2023-08-17 11:16:48 -0400

    implemented routes to create, update, and delete a tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,14 +45,49 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      res.status(200).json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tag) => {
+    return res.json(tag)
+  })
+  .catch((err) => {
+    res.status(400).json(err)
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag_data = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tag_data) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tag_data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
